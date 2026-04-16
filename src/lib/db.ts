302c33a1176ed949bea5e27536cdd226e171c900
Z User <z@container>
2026-04-16 18:48:37 +0000

    73edccd7-3793-43a2-92f0-b3ddd6d9de9d

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,20 +2,10 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
-  prismaVersion: string | undefined
 }
-
-// Force new PrismaClient if schema has changed
-const SCHEMA_VERSION = 'v2' // Bump this to force PrismaClient reload
 
 export const db =
-  (globalForPrisma.prisma && globalForPrisma.prismaVersion === SCHEMA_VERSION)
-    ? globalForPrisma.prisma
-    : new PrismaClient({
-        log: ['query'],
-      })
+  globalForPrisma.prisma ??
+  new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = db
-  globalForPrisma.prismaVersion = SCHEMA_VERSION
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
